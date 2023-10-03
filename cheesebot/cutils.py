ba83b6f818e92d1bb21b0c91abcc7c82b8a32f53
NotYou404 <dominik.reinartz@hotmail.com>
2023-10-03 19:35:09 +0200

    add eval

--- cheesebot/cutils.py
+++ cheesebot/cutils.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from typing import Generator, Optional
 
 import discord
 import templates
-from discord import utils
 import views
+from discord import utils
 
 
 def expand_audio_path(title: str | Path) -> Path:
@@ -41,3 +42,38 @@ async def send_voice_playback_error(
             "https://example.com", "Discord Support Server"
         )
     )
+
+
+def codeblockify(code: str, lang: Optional[str] = None) -> str:
+    """
+    Turn a code string into a markdown codeblock.
+
+    :param code: The code string.
+    :type code: str
+    :param lang: The lang used for syntax highlighting. If None, no syntax
+    highlighting will be available, defaults to None
+    :type lang: str, optional
+    :return: The code string as code block.
+    :rtype: str
+    """
+    return f"```{lang or ''}\n{code}\n```"
+
+
+def le_1024(text: str, replace: Optional[str] = None) -> str:
+    if replace is None:
+        replace = "*Too long to display*"
+    return text if len(text) <= 1024 else replace
+
+
+class OutputCollector:
+    def __init__(self) -> None:
+        self.content: list[str] = []
+
+    def write(self, text: str) -> None:
+        self.content.append(str(text))
+
+    def flush(self) -> None:
+        return
+
+    def __iter__(self) -> Generator:
+        yield from self.content
