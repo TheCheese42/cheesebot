611d28d670a9de341bf791cde064ebc296651dcf
Dominik <dominik.reinartz@proton.me>
2023-10-23 22:04:17 +0200

    worked on nitro command

--- cheesebot/cogs/fun.py
+++ cheesebot/cogs/fun.py
@@ -16,7 +16,18 @@ class Fun(discord.Cog):
         description="Claim your free Discord Nitro! Sponsored by CheeseBot "
                     "for new and loyal users.",
     )
-    @discord.
+    async def nitro(self, ctx: discord.ApplicationContext):
+        embed = discord.Embed(
+            title="Claim your free nitro **now**!",
+            description=f"Congratulations {ctx.author.mention}! You have the ultimate chance to claim your free nitro, sponsored by {ctx.bot.user.name}!",
+            author=discord.EmbedAuthor(
+                name=ctx.author.name,
+                icon_url=ctx.author.avatar.url if ctx.author.avatar else None,
+            )
+            timestamp=utils.utcnow(),
+        )
+        view = views.xyz # TODO
+        await ctx.reply(embed=embed, view=view)
 
 
 def setup(bot: discord.Bot):
